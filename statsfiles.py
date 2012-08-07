b7796fd5221942ab43d51a4ffdae12d84e86300c
Hydriz <admin@alphacorp.tk>
2012-08-07 22:45:07 +0800

    Whoops, forgot to import the relevant python libraries for boto

--- statsfiles.py
+++ statsfiles.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import boto
+from boto.s3.key import Key
 import os
 import sys
 
